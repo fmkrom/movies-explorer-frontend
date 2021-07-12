948cdc09fbd933f26b82244b746dc96b3d6115ef
fmkrom <fmkrom@yandex.ru>
2021-07-12 13:59:33 +0300

    feature: frontend deployed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,9 @@
 const URL ={
-    MY_BASE: 'http://localhost:3001',
+    MY_BASE: 'https://api.fmkrom-movies-exp.nomoredomains.icu',
     BEATFILM_MOVIES: 'https://api.nomoreparties.co/beatfilm-movies'
 }
 
 export default URL;
 
 //MY_BASE: 'https://api.fmkrom-movies-exp.nomoredomains.icu',
+//     MY_BASE: 'http://localhost:3001',
